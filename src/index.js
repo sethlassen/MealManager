e6be4b29d48bb914a14d83022faa651ca4e6ccd6
Seth Lassen <sethmlassen@gmail.com>
2021-04-16 15:28:53 -0700

    React setup complete, index.html rendering properly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+import App from "./app.js"
+
+ReactDOM.render(
+  <App />, document.getElementById('app')
+)
